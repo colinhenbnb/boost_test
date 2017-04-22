64b2ef74aef0afc5d1057e45c1436bb06a007482
heshaofu <heshaofu@douyu.tv>
2017-04-22 00:54:41 -0700

    add tcp test demo

--- asio_tcp_client.cpp
+++ asio_tcp_client.cpp
@@ -1,8 +1,8 @@
 /*************************************************************************
-	> File Name: asio_tcp_server.cpp
+	> File Name: asio_tcp_client.cpp
 	> Author: heshaofu
 	> Mail: heshaofu@douyu.tv
-	> Created Time: Sat 22 Apr 2017 12:21:33 AM PDT
+	> Created Time: Sat 22 Apr 2017 12:48:12 AM PDT
  ************************************************************************/
 
 #include<iostream>
@@ -18,19 +18,20 @@ int main()
         typedef ip::tcp::acceptor acceptor_type;
         typedef ip::tcp::endpoint endpoint_type;
         typedef ip::tcp::socket socket_type;
-        cout << "server start" <<endl;
+        typedef ip::address address_type;
+
+        cout << "client start" <<endl;
         io_service io;
-        acceptor_type acceptor(io,
-                               endpoint_type(ip::tcp::v4(), 6688));
-        cout << acceptor.local_endpoint().address() <<endl;
-        for (;;) 
-        {
-            socket_type sock(io);
-            acceptor.accept(sock);
-            cout << "client:";
-            cout << sock.remote_endpoint().address() <<endl;
-            sock.send(buffer("hello asio, welcome connect\n"));
-        }
+        socket_type sock(io);
+        endpoint_type ep(
+            address_type::from_string("127.0.0.1"), 6688
+            );
+        sock.connect(ep);
+        cout << sock.available()<<endl;
+        vector<char> str(sock.available()+1, 0);
+        sock.receive(buffer(str));
+        cout << "recv from " << sock.remote_endpoint().address();
+        cout<< &str[0] << endl;
         
     }
     catch (std::exception& e){
